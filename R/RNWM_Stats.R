a404efd2bfbf988219dd8ccfc57e5dab5d4c584b
Michael <barlage@ucar.edu>
2020-05-22 10:55:25 -0600

    add some documentation to main script

--- R/RNWM_Stats.R
+++ R/RNWM_Stats.R
@@ -1,8 +1,17 @@
 RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
+
   # This is the driver file or main Rscript
   # Make sure that the dataFrame contains model (mod) and observations (obs)
 
   #Assume it doesn't have missing data for now
+  
+  #------------------------------------#
+  # Model calculations
+  #------------------------------------#
+
+    #------------------------------------#
+    # Calculate baseflow (mod_BF)
+    #------------------------------------#
 
   mod_BF<-BaseFlowSeparation(dataFrame$mod,
                                        bf_method=bf_method,
@@ -10,11 +19,26 @@ RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
                                        c=quantile(streamflow,probs = 0.25, na.rm=T),
                                        filter_parameter=0.925,
                                        passes=1)
+
+    #------------------------------------#
+    # Calculate baseflow index (mod_BFI)
+    #------------------------------------#
+
   mod_BFI<-BaseFlowIndex(mod_BF$qflow, mod_BF$bt, na.rm=T)
+
+    #------------------------------------#
+    # Calculate events (mod_nev)
+    #------------------------------------#
 
   mod_nev<-StormEvents(mod_BF$qft,qlimit = qlimit)
 
   #------------------------------------#
+  # Obs calculations
+  #------------------------------------#
+
+    #------------------------------------#
+    # Calculate baseflow (obs_BF)
+    #------------------------------------#
 
   obs_BF<-BaseFlowSeparation(dataFrame$obs,
                                        bf_method=bf_method,
@@ -23,14 +47,38 @@ RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
                                        filter_parameter=0.925,
                                        passes=1)
 
+    #------------------------------------#
+    # Calculate baseflow index (obs_BFI)
+    #------------------------------------#
+
   obs_BFI<-BaseFlowIndex(obs_BF$qflow, obs_BF$bt, na.rm=T)
+
+    #------------------------------------#
+    # Calculate events (obs_nev)
+    #------------------------------------#
 
   obs_nev<-StormEvents(obs_BF$qft,qlimit = qlimit)
 
 
-  #-------------------------------------#
+  #------------------------------------#
+  # Metrics requiring model and obs
+  #------------------------------------#
+
+    #------------------------------------#
+    # Calculate root mean square error (RMSE)
+    #------------------------------------#
+
   RMSE <-RMSE(dataFrame$mod,dataFrame$obs,na.rm=T)
+
+    #------------------------------------#
+    # Calculate percent bias (PBIAS)
+    #------------------------------------#
+
   PBIAS<-PBias(dataFrame$mod,dataFrame$obs,na.rm=T)
+
+  #------------------------------------#
+  # Populate statistics object
+  #------------------------------------#
 
   Stat_obj<-list()
   Stat_obj$Obs_BaseFlow<-obs_BF$bt
