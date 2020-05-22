bdcc358b7081fff8c369cc4fbce5c4364bfd6a27
prasanthvkrishna <prasanth@ucar.edu>
2020-05-22 11:27:49 -0600

    resolved conflict

--- R/RNWM_Stats.R
+++ R/RNWM_Stats.R
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,cor_method='pearson',cor_use='everything',na.rm=T){
-=======
-RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
+RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,cor_use='everything',na.rm=T){
 
->>>>>>> a404efd2bfbf988219dd8ccfc57e5dab5d4c584b
   # This is the driver file or main Rscript
   # Make sure that the dataFrame contains model (mod) and observations (obs)
 
   #Assume it doesn't have missing data for now
-  
+
   #------------------------------------#
   # Model calculations
   #------------------------------------#
@@ -16,25 +12,27 @@ RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
     #------------------------------------#
     # Calculate baseflow (mod_BF)
     #------------------------------------#
-
-  mod_BF<-BaseFlowSeparation(dataFrame$mod,
+    mod_BF<-BaseFlowSeparation(dataFrame$mod,
                                        bf_method=bf_method,
                                        k=0.93,
                                        c=quantile(streamflow,probs = 0.25, na.rm=T),
                                        filter_parameter=0.925,
                                        passes=1)
 
+
+
     #------------------------------------#
     # Calculate baseflow index (mod_BFI)
     #------------------------------------#
+    mod_BFI<-BaseFlowIndex(mod_BF$qflow, mod_BF$bt, na.rm=T)
 
-  mod_BFI<-BaseFlowIndex(mod_BF$qflow, mod_BF$bt, na.rm=T)
 
     #------------------------------------#
     # Calculate events (mod_nev)
     #------------------------------------#
+    mod_nev<-StormEvents(mod_BF$qft,qlimit = qlimit)
 
-  mod_nev<-StormEvents(mod_BF$qft,qlimit = qlimit)
+
 
   #------------------------------------#
   # Obs calculations
@@ -43,8 +41,7 @@ RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
     #------------------------------------#
     # Calculate baseflow (obs_BF)
     #------------------------------------#
-
-  obs_BF<-BaseFlowSeparation(dataFrame$obs,
+    obs_BF<-BaseFlowSeparation(dataFrame$obs,
                                        bf_method=bf_method,
                                        k=0.93,
                                        c=quantile(streamflow,probs = 0.25, na.rm=T),
@@ -55,25 +52,17 @@ RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
     # Calculate baseflow index (obs_BFI)
     #------------------------------------#
 
-  obs_BFI<-BaseFlowIndex(obs_BF$qflow, obs_BF$bt, na.rm=T)
+    obs_BFI<-BaseFlowIndex(obs_BF$qflow, obs_BF$bt, na.rm=T)
 
     #------------------------------------#
     # Calculate events (obs_nev)
     #------------------------------------#
 
-  obs_nev<-StormEvents(obs_BF$qft,qlimit = qlimit)
+    obs_nev<-StormEvents(obs_BF$qft,qlimit = qlimit)
+
+    #-------------------------------------#
 
 
-<<<<<<< HEAD
-  #-------------------------------------#
-  RMSE  <-RMSE(dataFrame$mod,dataFrame$obs,na.rm=T)
-  NRMSE <-PBias(dataFrame$mod,dataFrame$obs,na.rm=T)
-  PBIAS <-PBias(dataFrame$mod,dataFrame$obs,na.rm=T)
-  KGE   <-KGE(dataFrame$mod,dataFrame$obs, na.rm=TRUE, s.r=1, s.alpha=1, s.beta=1)
-  R2    <-RSquared(dataFrame$mod,dataFrame$obs)
-  NSE   <-NSE(dataFrame$mod,dataFrame$obs, nullModel=mean(dataFrame$obs, na.rm=na.rm), na.rm=TRUE)
-  CORRELATION<-cor(dataFrame$mod,dataFrame$obs,method = cor_method,use=cor_use)
-=======
   #------------------------------------#
   # Metrics requiring model and obs
   #------------------------------------#
@@ -82,44 +71,48 @@ RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
     # Calculate root mean square error (RMSE)
     #------------------------------------#
 
-  RMSE <-RMSE(dataFrame$mod,dataFrame$obs,na.rm=T)
+    RMSE <-RMSE(dataFrame$mod,dataFrame$obs,na.rm=T)
+
+    #------------------------------------#
+    # Calculate normalized root mean square error (NRMSE)
+    #------------------------------------#
+
+    NRMSE <-NRMSE(dataFrame$mod,dataFrame$obs,na.rm=T)
 
     #------------------------------------#
     # Calculate percent bias (PBIAS)
     #------------------------------------#
 
-  PBIAS<-PBias(dataFrame$mod,dataFrame$obs,na.rm=T)
->>>>>>> a404efd2bfbf988219dd8ccfc57e5dab5d4c584b
+    PBIAS<-PBias(dataFrame$mod,dataFrame$obs,na.rm=T)
 
     #------------------------------------#
     # Calculate NSE (NSE)
     #------------------------------------#
-    
-  ### function to be added here
+    NSE   <-NSE(dataFrame$mod,dataFrame$obs, nullModel=mean(dataFrame$obs, na.rm=na.rm), na.rm=TRUE)
+
 
     #------------------------------------#
     # Calculate KGE (KGE)
     #------------------------------------#
+    KGE   <-KGE(dataFrame$mod,dataFrame$obs, na.rm=TRUE, s.r=1, s.alpha=1, s.beta=1)
 
-  ### function to be added here
 
     #------------------------------------#
     # Calculate correlation (R2)
     #------------------------------------#
+    R2    <-RSquared(dataFrame$mod,dataFrame$obs)
 
-  ### function to be added here
 
     #------------------------------------#
     # Calculate Pearson R (Pearson_R)
     #------------------------------------#
-
-  ### function to be added here
+    COR_Pearson<-cor(dataFrame$mod,dataFrame$obs,method ='pearson',use=cor_use)
 
     #------------------------------------#
     # Calculate Spearman R (Spearman_R)
     #------------------------------------#
+    COR_Spearman<-cor(dataFrame$mod,dataFrame$obs,method ='spearman',use=cor_use)
 
-  ### function to be added here
 
   #------------------------------------#
   # Populate statistics object
@@ -141,7 +134,8 @@ RNWM_Stats<-function(dataFrame,bf_method='Eckhardt',qlimit=0,na.rm=T){
   Stat_obj$NRMSE <- NRMSE
   Stat_obj$KGE <-KGE
   Stat_obj$R2  <- R2
-  Stat_obj$CORRELATION<-CORRELATION
+  Stat_obj$COR_Pearson<-COR_Pearson
+  Stat_obj$COR_Spearman<-COR_Spearman
   Stat_obj$NSE <- NSE
 
   return(Stat_obj)
