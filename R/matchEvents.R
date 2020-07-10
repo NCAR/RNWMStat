29410922680ba0e9d67302a635099595f0ed71e4
Yuqiong Liu <yuqiong.liu@noaa.gov>
2020-07-10 21:51:38 +0000

    similfied output from event matching

--- R/matchEvents.R
+++ R/matchEvents.R
@@ -1,9 +1,11 @@
-# matched observed events with modelled events
+# Match observed (compound) events with model events
 
-matchEvents <- function(eventsAllMod, eventsCompoundMod,
+matchEvents <- function(data_mod, eventsAllMod, eventsCompoundMod,
   eventsCompoundObs,maxDist=48) {
 
 ###### Inputs ##############
+# data_mod: data.frame for model streamflow, 1st column is time in POSIXct format,
+#       2nd column is the flow values
 # eventsAllMod: data frame listing all single-peak events for model
 #    streamflow (identified by eventIdentification.R)
 # eventsCompoundMod: data frame of compound events for model streamflow
@@ -13,21 +15,25 @@ matchEvents <- function(eventsAllMod, eventsCompoundMod,
 #    non-snow basins and 7*24 (i.e., one week) for snow basins
 
 ##### Output ##############
-# data frame with infomation on events matching. It has the same
-# number of rows as eventsCompoundObs
-#   1st column: index of observed events (in eventsCompoundObs)
-#   2nd column: match category 
+# data frame with columns corresponding to the start, peak, and end times of
+# matched observed and model events. It has the same number of rows as 
+#   eventsCompoundObs
+#   1st - 3rd columns: start, peak and end times of observed events
+#   4th - 6th columns: start, peak and end times of model events 
+#   7th column: match category 
 #       1 = matched with eventsCompoundMod
 #       2 = matched with eventsAllMod
 #       3 = missed by model
-#   3rd column: index of matched model events in eventsCompoundMod
-#   4th column: index of matched model events in eventsAllMod
- 
 
 no1 <- nrow(eventsCompoundObs)
 nm1 <- nrow(eventsCompoundMod)
 nm2 <- nrow(eventsAllMod) 
 
+if (no1==0 | (nm1+nm2)==0) { 
+  print("WARNING: no events to match")
+  return(data.frame())
+}
+ 
 dfMatch <- data.frame(ix_obs=1:no1,match=rep(NA,no1),
   ix_mod1=rep(NA,no1), ix_mod2=rep(NA,no1))
 
@@ -59,6 +65,37 @@ for (k1 in 1:no1) {
   }
 }
 
-dfMatch
+# assemble matched events of obs and mod
+peak_obs <- eventsCompoundObs$peak
+start_obs <- eventsCompoundObs$start
+end_obs <- eventsCompoundObs$end
+
+peak_mod <- peak_obs
+start_mod <- start_obs
+end_mod <- end_obs
+
+ix1 <- which(dfMatch$match==1)
+if (length(ix1)>0) {
+  peak_mod[ix1] <- eventsCompoundMod$peak[dfMatch$ix_mod1[ix1]]
+  start_mod[ix1] <- eventsCompoundMod$start[dfMatch$ix_mod1[ix1]]
+  end_mod[ix1] <- eventsCompoundMod$end[dfMatch$ix_mod1[ix1]]
+}
+ix1 <- which(dfMatch$match==2)
+if (length(ix1)>0) {
+  peak_mod[ix1] <- eventsAllMod$peak[dfMatch$ix_mod2[ix1]]
+  start_mod[ix1] <- eventsAllMod$start[dfMatch$ix_mod2[ix1]]
+  end_mod[ix1] <- eventsAllMod$end[dfMatch$ix_mod2[ix1]]
+}
+ix1 <- which(dfMatch$match==3)
+if (length(ix1)>0) {
+names(data_mod) <- c("time","value")
+for (k1 in ix1) {
+tmp <- subset(data_mod,time>=eventsCompoundObs$start[k1] & time<=eventsCompoundObs$end[k1])
+peak_mod[k1] <- tmp$time[which.max(tmp$value)]
+}}
+
+eventsMatched <- data.frame(start_obs,peak_obs,end_obs,start_mod,peak_mod,end_mod)
+eventsMatched$match <- dfMatch$match
+eventsMatched
 
 }
