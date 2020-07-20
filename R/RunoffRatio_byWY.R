ec57f988041c74b271e8ac2f284f9e34b4a1ab83
Yuqiong Liu <yuqiong.liu@noaa.gov>
2020-07-20 14:09:47 +0000

    runoff ration by water year

--- R/RunoffRatio_byWY.R
+++ R/RunoffRatio_byWY.R
@@ -6,6 +6,7 @@ RunoffRatio_byWY<-function(dfRR,WS_area){
 
   dt1 <- as.data.table(dfRR)
   names(dt1) <- c("Q","PCP","Date")
+  print(class(dt1))
   dt1[, Qmm:=Q*3600/WS_area*1000]
   dt1[, year:=as.integer(format(Date,"%Y"))]
   dt1[, month:=as.integer(format(Date,"%m"))]
